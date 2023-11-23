8a22e1237033321e3657545f8355963d994825c3
Mohit6577 <Mohitchoudhary657@gmail.com>
2023-11-23 22:35:50 +0530

    add delete user functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -35,3 +35,17 @@ export const updateUser= async(req,res,next) =>{
        next(error) 
     }
 }
+
+//Delete user
+
+export const deleteUser= async (req,res,next)=>{
+    if(req.user.id !==req.user.id){
+        return (errorHandler(401,'You can delete only yout account'));
+    }
+    try {
+        await User.findByIdAndDelete(req.params.id);
+        res.status(200).json('User has been deleted...')
+    } catch (error) {
+        next(error);
+    }
+}
